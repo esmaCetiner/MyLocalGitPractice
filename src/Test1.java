e381001d41783c4cedf3ddb233539dbb57e3e3c4
esmaCetiner <145158382+esmaCetiner@users.noreply.github.com>
2023-09-18 21:26:37 +0200

    Updated Test1.java at REMOTE repo

--- src/Test1.java
+++ src/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
         System.out.println("Test1");
         System.out.println("ADD LOCALLY!!!");
         System.out.println("ADDED LOCALLY, pushed with GIT MENU!!!");
+        // BURASI REMOTE REPORDA GITHUPDA EKLENDI!!!
     }
 }
